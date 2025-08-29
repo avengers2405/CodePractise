9f14641a0d7eb77d4cdedee7d3463cf09a29c3f4
Akshit <akshitmishra24@gmail.com>
2025-08-30 01:23:09 +0530

    dynamincally take port from command line for server

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag" // this library is used to take flag values from cmd while running the file. it used to take port arg below, default being 5000
 	"fmt"
 	"net/http"
 	"time"
@@ -8,20 +9,26 @@ import (
 	"github.com/avengers2405/CodePractise/backend/app"
 )
 
-func main(){
+func main() {
+
+	var port int
+	flag.IntVar(&port, "port", 5000, "port which server listens to")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running from the app");
+	app.Logger.Println("we are running from the app")
 
 	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
-		Addr: ":5000",
-		IdleTimeout: 5*time.Second,
-		ReadTimeout: time.Minute,
-		WriteTimeout: 15*time.Second,
+		// Addr: ":5000",
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  time.Minute,
+		WriteTimeout: 15 * time.Second,
 	}
 
 	err = server.ListenAndServe()
